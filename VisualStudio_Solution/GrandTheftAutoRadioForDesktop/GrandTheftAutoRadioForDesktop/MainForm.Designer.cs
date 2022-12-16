fb62e7600693472802a8735a630e76db845e28fc
LiamvanLeusden <liamvanleusden@gmail.com>
2022-12-16 16:54:05 +0100

    Removed GITIGNORE
    GITIGNORE was causing issues where I could not open my solution on a differenct computer. I plan to add a new GITIGNORE soon.

--- VisualStudio_Solution/GrandTheftAutoRadioForDesktop/GrandTheftAutoRadioForDesktop/MainForm.Designer.cs
+++ VisualStudio_Solution/GrandTheftAutoRadioForDesktop/GrandTheftAutoRadioForDesktop/MainForm.Designer.cs
@@ -34,7 +34,6 @@ namespace GrandTheftAutoRadioForDesktop
             this.btnChooseFolder = new System.Windows.Forms.Button();
             this.dlgChooseFolder = new System.Windows.Forms.FolderBrowserDialog();
             this.txtChooseFolder = new System.Windows.Forms.TextBox();
-            this.player = new AxWMPLib.AxWindowsMediaPlayer();
             this.btnStart = new System.Windows.Forms.Button();
             this.cbbSelectStation = new System.Windows.Forms.ComboBox();
             this.lblStaticSelectStation = new System.Windows.Forms.Label();
@@ -68,7 +67,10 @@ namespace GrandTheftAutoRadioForDesktop
             this.dlgFileSave = new System.Windows.Forms.SaveFileDialog();
             this.trcVolume = new System.Windows.Forms.TrackBar();
             this.lblStaticVolume = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.pctRadioLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trcPreferencesSong)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trcPreferencesStinger)).BeginInit();
@@ -76,6 +78,8 @@ namespace GrandTheftAutoRadioForDesktop
             ((System.ComponentModel.ISupportInitialize)(this.trcPreferencesAdvertisement)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trcPreferencesNewsItem)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trcVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnChooseFolder
@@ -104,18 +108,6 @@ namespace GrandTheftAutoRadioForDesktop
             this.txtChooseFolder.Size = new System.Drawing.Size(695, 21);
             this.txtChooseFolder.TabIndex = 1;
             this.txtChooseFolder.Text = "<No folder chosen, GTA radio will not work!>";
-            // 
-            // player
-            // 
-            this.player.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.player.Enabled = true;
-            this.player.Location = new System.Drawing.Point(0, 383);
-            this.player.Name = "player";
-            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
-            this.player.Size = new System.Drawing.Size(800, 65);
-            this.player.TabIndex = 2;
-            this.player.Visible = false;
-            this.player.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.player_PlayStateChange);
             // 
             // btnStart
             // 
@@ -443,9 +435,9 @@ namespace GrandTheftAutoRadioForDesktop
             // 
             // btnDebugShowPlayer
             // 
-            this.btnDebugShowPlayer.Location = new System.Drawing.Point(286, 256);
+            this.btnDebugShowPlayer.Location = new System.Drawing.Point(15, 301);
             this.btnDebugShowPlayer.Name = "btnDebugShowPlayer";
-            this.btnDebugShowPlayer.Size = new System.Drawing.Size(232, 23);
+            this.btnDebugShowPlayer.Size = new System.Drawing.Size(517, 23);
             this.btnDebugShowPlayer.TabIndex = 33;
             this.btnDebugShowPlayer.Text = "Toggle show WMP (debug)";
             this.btnDebugShowPlayer.UseVisualStyleBackColor = true;
@@ -514,18 +506,71 @@ namespace GrandTheftAutoRadioForDesktop
             this.lblStaticVolume.TabIndex = 37;
             this.lblStaticVolume.Text = "Volume:";
             // 
+            // player
+            // 
+            this.player.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(0, 383);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(800, 65);
+            this.player.TabIndex = 2;
+            this.player.Visible = false;
+            this.player.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.player_PlayStateChange);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(504, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "0";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(283, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 13);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "Set probability of occuring: Advertisement";
+            this.label2.Visible = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(286, 261);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(212, 45);
+            this.trackBar1.SmallChange = 5;
+            this.trackBar1.TabIndex = 38;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Visible = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
             this.ClientSize = new System.Drawing.Size(800, 448);
+            this.Controls.Add(this.btnDebugShowPlayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.lblStaticVolume);
             this.Controls.Add(this.btnToggleSettingsVisibility);
             this.Controls.Add(this.trcVolume);
             this.Controls.Add(this.btnStart1);
             this.Controls.Add(this.lblError);
-            this.Controls.Add(this.btnDebugShowPlayer);
             this.Controls.Add(this.lblDisplayValueStinger);
             this.Controls.Add(this.lblDisplayValueAdvertisement);
             this.Controls.Add(this.lblDisplayValueQuote);
@@ -557,7 +602,6 @@ namespace GrandTheftAutoRadioForDesktop
             this.Name = "MainForm";
             this.Text = "GrandTheftAutoRadioForDesktop";
             this.Load += new System.EventHandler(this.MainForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pctRadioLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trcPreferencesSong)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trcPreferencesStinger)).EndInit();
@@ -565,6 +609,8 @@ namespace GrandTheftAutoRadioForDesktop
             ((System.ComponentModel.ISupportInitialize)(this.trcPreferencesAdvertisement)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trcPreferencesNewsItem)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trcVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -609,6 +655,9 @@ namespace GrandTheftAutoRadioForDesktop
         private System.Windows.Forms.SaveFileDialog dlgFileSave;
         private System.Windows.Forms.TrackBar trcVolume;
         private System.Windows.Forms.Label lblStaticVolume;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
